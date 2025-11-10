373f2b5b1c66ecd1a50ff384698e04d762307ef3
archit0111 <sarchit0111@gmail.com>
2025-11-11 00:00:04 +0530

    Naming convention corrected

--- Expancetraker/expancetraker/src/ExpanceTraker.jsx
+++ Expancetraker/expancetraker/src/ExpanceTraker.jsx
@@ -2,34 +2,34 @@ import './ExpanceTraker.css'
 import { useState } from 'react'
 
 function ExpanceTraker(){
-    const [Newexp,setNewExp]=useState("");
-    const [Newexpam,setNewexpam]=useState("");
-    const [T_exp,setT_exp]=useState({});
+    const [NewExpance,setNewExpance]=useState("");
+    const [NewExpanceAmount,setNewExpanceAmount]=useState("");
+    const [TotalExpance,setTotalExpance]=useState({});
     const [display,setdisplay]=useState(false);
-    const [exp_no,setexp_no]=useState(false);
+    const [NumberOfExpances,setNumberOfExpances]=useState(false);
 
     function handeladd(){
-        if(Newexp===""){
+        if(NewExpance===""){
             alert("Enter your expance...");
         }
-        if(Newexpam===""||isNaN(Newexpam)){
+        if(NewExpanceAmount===""||isNaN(NewExpanceAmount)){
             alert("Enter the expance's amount...");
         }
-        if(Newexp!=""&&Newexpam!=""){
-            setT_exp({...T_exp,[Newexp]:Newexpam});
+        if(NewExpance!=""&&NewExpanceAmount!=""){
+            setTotalExpance({...TotalExpance,[NewExpance]:NewExpanceAmount});
             setdisplay(true);
-            setNewExp("");
-            setNewexpam("");
+            setNewExpance("");
+            setNewExpanceAmount("");
         }
 
     }
 
     function handelDel(k){
-        const copy = {...T_exp};
+        const copy = {...TotalExpance};
         delete copy[k];
         setT_exp(copy);
-        if(Object.entries(T_exp).length === 1){
-            setexp_no(true);
+        if(Object.entries(TotalExpance).length === 1){
+            setNumberOfExpances(true);
         }
     }
     
@@ -39,26 +39,26 @@ function ExpanceTraker(){
         <div className="addbox">
             <div className="add_heading">Enter new expance:</div>
             <div className="add">
-                <input type="text" placeholder='Expance ?' id='expance' value={Newexp} onChange={(e)=>{setNewExp(e.target.value)}} />
-                <input type="Number" placeholder='Amount ?' id='amount' value={Newexpam} onChange={(e)=>{setNewexpam(e.target.value)}}/>
+                <input type="text" placeholder='Expance ?' id='expance' value={NewExpance} onChange={(e)=>{setNewExpance(e.target.value)}} />
+                <input type="Number" placeholder='Amount ?' id='amount' value={NewExpanceAmount} onChange={(e)=>{setNewExpanceAmount(e.target.value)}}/>
                 <button id='add_btn'onClick={handeladd}>Add</button>
             </div>
         </div>
             {
                 display?
         <div className="display">{
-                Object.keys(T_exp).map(key=>(
+                Object.keys(TotalExpance).map(key=>(
                     <div id='dis_row' key={key}>
                     <div id='exp_box'>
                         {key}
                     </div>
                     <div id='am_box'>
-                        {T_exp[key]}
+                        {TotalExpance[key]}
                     </div>
                     <button id='del' onClick={()=>handelDel(key)}>Delete</button>
                     </div>
                 ))}
-                <div className="nothing_to_dis">{exp_no?<div id='ntd'>There is no expances added by you!</div>:null}</div>
+                <div className="nothing_to_dis">{NumberOfExpances?<div id='ntd'>There is no expances added by you!</div>:null}</div>
         </div>:null
             }
 
