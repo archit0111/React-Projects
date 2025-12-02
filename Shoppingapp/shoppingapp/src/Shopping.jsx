be7c233d0971875d99dcb725daeb5ccf773adb75
archit0111 <sarchit0111@gmail.com>
2025-12-03 02:00:21 +0530

    Prodcut_listed

--- Shoppingapp/shoppingapp/src/Shopping.jsx
+++ Shoppingapp/shoppingapp/src/Shopping.jsx
@@ -1,7 +1,71 @@
 import { useState } from "react";
+// import img from '/ts-1.webpg'
 
 function Shopping(){
     const [options,setOptions] = useState(false);
+
+    const products ={
+        id_1:{
+        name: "T Shirt",
+        img: '/ts-1.webp',
+        prize: 500,
+        type:["tshirt","cotton", "cloths", "casual", "man"]
+        },
+        id_2:{
+        name: "Formal Shoe",
+        img: '/shoe-1.webp',
+        prize: 2500,
+        type:["shoe","formal", "shoe", "black", "man"]
+        },
+        id_3:{
+        name: "Casual Shoe",
+        img: '/shoe-2.webp',
+        prize: 1500,
+        type:["shoe","casual", "shoe", "blue", "man"]
+        },
+        id_4:{
+        name: "Cap",
+        img: '/cap-1.webp',
+        prize: 500,
+        type:["cap","casual","black"]
+        },
+        id_5:{
+        name: "Cap",
+        img: '/cap-2.webp',
+        prize: 550,
+        type:["cap","casual","white"]
+        },
+        id_6:{
+        name: "Headphone",
+        img: '/headphone-1.webp',
+        prize: 1959,
+        type:["headphone","electronics", "silver", "boat"]
+        },
+        id_7:{
+        name: "Headphone",
+        img: '/headphone-2.webp',
+        prize: 2590,
+        type:["headphone","electronics", "black", "jlb"]
+        },
+        id_8:{
+        name: "Laptop asus",
+        img: '/lap-1.webp',
+        prize: 50099,
+        type:["laptop","asus", "black", "electronics"]
+        },
+        id_9:{
+        name: "Laptop aser",
+        img: '/lap-2.webp',
+        prize: 112999,
+        type:["laptop","aser", "black", "electronics"]
+        },
+        id_10:{
+        name: "Sari",
+        img: '/sari.webp',
+        prize: 4999,
+        type:["sari","cloth", "women", "pink"]
+        }
+    }
 
     function handleOnclick(){
         if(!options){
@@ -39,8 +103,22 @@ function Shopping(){
             </div>
         </section>
         <main className='p-2.5'>
-            <div className='grid-cols-3 grid-rows-5'>
-
+            <div className='xl:p-8 sm:p-2 m-2 gap-12 md:grid md:grid-cols-3'>
+                {Object.entries(products).map(([key,value])=>(
+                    <div className='rounded-2xl overflow-hidden p-2 sm:p-5 bg-indigo-200 sm:mb-40 mb-30 max-h-max' value={key}>
+                       <div className="h-6/9 place-content-center justify-items-center"><div className="h-fit"><img src={value.img} alt={value.name}/></div></div>
+                        <section className="sm:p-4 p-3 flex justify-between font-bold sm:text-xl text-lg ">
+                            <p>{value.name}</p>
+                            <p>{`₹${value.prize}`}</p>
+                        </section>
+                        <section className="sm:p-4 text-sm sm:text-lg -mt-1.5">
+                            <button className='font-medium bg-indigo-400 hover:bg-indigo-500 p-2 block place-self-center rounded-2xl w-3/5 sm:w-5/6 mb-2 hover:shadow-md hover:scale-103 hover:shadow-indigo-500 '>Shop Now</button>
+                            <button className='font-medium bg-indigo-400 hover:bg-indigo-500 p-2 block place-self-center rounded-2xl w-3/5 sm:w-5/6 mb-2 hover:shadow-md hover:scale-103 hover:shadow-indigo-500'>Add to cart</button>
+                            <button className='font-medium bg-indigo-400 hover:bg-indigo-500 p-2 block place-self-center rounded-2xl w-3/5 sm:w-5/6 mb-4 hover:shadow-md hover:scale-103 hover:shadow-indigo-500'>Add to Wishlist</button>
+                        </section>
+                    </div>
+                ))}
+                
             </div>
         </main>
         </>
